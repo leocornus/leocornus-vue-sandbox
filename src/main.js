3d5c079bdc2721f89c93aa162d0a5a5d83c6fa58
Sean Chen <sean.chen@leocorn.com>
2018-03-30 14:12:34 +0000

    get the light bootstrap dashboard to start with.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import App from './App.vue'
 
-Vue.config.productionTip = false
+// LightBootstrap plugin
+import LightBootstrap from './light-bootstrap-main'
+
+// router setup
+import routes from './routes/routes'
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(LightBootstrap)
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: 'nav-item active'
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App),
+  router
 })
