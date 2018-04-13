3dbf5e51998493ad86a1e008c6cf936ab0575734
Sean Chen <sean.chen@leocorn.com>
2018-04-13 00:37:50 +0000

    remove the un-necessary code.

--- src/components/UIComponents/Cards/ChartCard.vue
+++ src/components/UIComponents/Cards/ChartCard.vue
@@ -12,12 +12,9 @@
   </div>
 </template>
 <script>
-  import Card from './Card.vue'
-
   export default {
     name: 'chart-card',
     components: {
-      Card
     },
     props: {
       chartType: {
