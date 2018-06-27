62e68298252953c1ccdd1f4883b3e119de1ed67e
Sean Chen <sean.chen@leocorn.com>
2018-06-27 14:31:08 +0000

    the sample config file for local settings.

---
+++ src/local-settings-sample.js
@@ -0,0 +1,29 @@
+/**
+ * some sample data for local settings
+ * copy this file to local-settings.js and update with your local settings.
+ */
+export default {
+
+  install(Vue) {
+    Vue.mixin({
+
+      created: function() {
+
+        this.$localSettings = {
+
+            /**
+             * local settings for real estate try out.
+             */
+            reSearchUrl: 'https://search.example.com/rest/searchApi/search',
+
+            /**
+             * lcoal settings for SharePoint Online try out.
+             */
+            targetSource: 'https://onegroup.sharepoint.com',
+            sharepointSite: '/sites/qasite',
+            accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1N'
+        }
+      }
+    })
+  }
+}
