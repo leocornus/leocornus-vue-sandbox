beffdfc0ba29fe82f0490416bec5dac489053c2c
Sean Chen <sean.chen@leocorn.com>
2018-09-12 14:57:46 +0000

    minor fix, the padding issue

--- src/components/UIComponents/ListingDetails.vue
+++ src/components/UIComponents/ListingDetails.vue
@@ -1,7 +1,7 @@
 <template>
 
 <b-card class="mb-1">
-<b-card-header :id="listingID">
+<b-card-header :id="listingID" class="p-0">
   <h5 class="mb-0">
     <b-button class="btn-link" v-b-toggle="collapseID">
       {{caption}}
