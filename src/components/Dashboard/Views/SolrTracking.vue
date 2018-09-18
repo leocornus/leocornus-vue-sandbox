b3b092bb241cc164b223767c26d2f520f9cf2fcd
Sean Chen <sean.chen@leocorn.com>
2018-09-18 03:10:32 +0000

    update to use more generic variable name

--- src/components/Dashboard/Views/SolrTracking.vue
+++ src/components/Dashboard/Views/SolrTracking.vue
@@ -4,12 +4,12 @@
 
     <b-input-group class="mb-2">
       <b-dropdown right text="Choose Tracking">
-        <b-dropdown-item v-for="(queue, index) in eventQueues" :key="index"
-                         v-on:click="switchQueue(queue.name, index)"
-        >{{queue.name}}</b-dropdown-item>
+        <b-dropdown-item v-for="(collection, index) in collections" :key="index"
+                         v-on:click="switchQueue(collection.name, index)"
+        >{{collection.name}}</b-dropdown-item>
       </b-dropdown>
       <b-input-group-append>
-        <span id="restBaseUrl-addon" class="input-group-text">{{queueLabel}}</span>
+        <span id="restBaseUrl-addon" class="input-group-text">{{collectionLabel}}</span>
       </b-input-group-append>
       <b-form-input type="text" class="form-control" id="restBaseUrl"
              aria-describedby="restBaseUrl-addon"
@@ -17,18 +17,7 @@
              placeholder="RESTful API base URL https://www.rest.com"/>
       <b-input-group-append>
          <b-button variant="outline-primary"
-            v-on:click="loadEvents">Refresh</b-button>
-      </b-input-group-append>
-    </b-input-group>
-
-    <b-input-group size="lg" class="mb-2 d-none">
-      <b-form-input type="text" id="inputQuery"
-             v-model="query"
-             v-on:keyup.enter.native="simpleSearch"
-             placeholder="Search b-form-group for..."/>
-      <b-input-group-append>
-        <b-button variant="outline-primary" size="lg"
-                v-on:click="simpleSearch">Search!</b-button>
+            v-on:click="loadItems">Refresh</b-button>
       </b-input-group-append>
     </b-input-group>
 
@@ -88,10 +77,11 @@ export default {
     data() {
       return {
         restBaseUrl: 'http://search.example.com',
-        // available event queues.
-        eventQueues: [],
+        // available collections.
+        collections: [],
         // label for the current event queue
         eventLabel: "",
+
         query: '*:*',
         // default facet field is empty.
         facetFields: "",
@@ -137,7 +127,7 @@ export default {
       currentPage: function(newValue) {
           // reload page.
           console.log("Watching currentPage: " + newValue);
-          this.loadEvents();
+          this.loadItems();
       }
     },
 
@@ -145,7 +135,7 @@ export default {
 
         /**
          */
-        loadEvents() {
+        loadItems() {
 
             var vm = this;
 
@@ -220,17 +210,17 @@ export default {
          * hook on the click event on queue selection dropdown.
          * we could use the inline JavaScript statement to pass the queue name.
          * here is a example:
-         * <b-dropdown-item v-for="(queue, index) in eventQueues"
-         *        v-on:click="switchQueue(queue.name, index)">
+         * <b-dropdown-item v-for="(collection, index) in collections"
+         *        v-on:click="switchQueue(collection.name, index)">
          */
         switchQueue(queueName, index) {
 
             //console.log("["+ index + "] " + queueName);
 
-            this.queueLabel = this.eventQueues[index].name;
-            this.restBaseUrl = this.eventQueues[index].url;
+            this.collectionLabel = this.collections[index].name;
+            this.restBaseUrl = this.collections[index].url;
             this.currentPage = 1;
-            this.loadEvents();
+            this.loadItems();
         },
 
         /**
@@ -245,109 +235,6 @@ export default {
             } else {
                 return oneDoc['id'];
             }
-        },
-
-        /**
-         * simple search function to demonstrate Solr search function.
-         */
-        simpleSearch() {
-
-            var self = this;
-            //console.log('I am in...');
-            solr.config.baseUrl = self.restBaseUrl;
-            solr.ping();
-
-            self.resultSummary = "Searching ...";
-            // set the results to null for hiding the whole section.
-            self.results = null;
-            self.facets = null;
-            self.stats = null;
-
-            // check the query, 
-            if(!this.query) {
-              // reset the query to search everything!
-              this.query="*:*";
-            }
-
-            // make sure we have at less one statistics.
-            //if(self.facetFields.includes('statistics')) {
-            //} else {
-            //    // add the statistics on listvalue_i
-            //    self.facetFields = self.facetFields + 
-            //      ",listvalue_i(statistics=true)";
-            //}
-
-            // post payload, it will be the query parameters here:
-            // This is the JSON request payload.
-            var postPayload = {
-                //workflow: "customsearch",
-                query: this.query,
-                limit: 250,
-                offset: 0,
-                //fields: [".id","title","table","avgScore"],
-                //sort: ["title:ASC"],
-                // facets: ["table", "city", "agentname"],
-                //facets: self.facetFields.split(',')
-                facet: "on",
-                "facet.field": "project_id",
-                "facet.field": "customer_id"
-            };
-
-            // the parameters for query.
-            // we will use Object assign to merge them all together.
-            var params = Object.assign({
-              rows: 25,
-              start: 0,
-              sort: self.sort
-            }, self.getFacetFields(), self.getFieldList(), 
-            self.getFilterQuery());
-
-            // this will show how to use query parameters in a JSON request.
-            var postParams = {
-                query: this.query,
-                // we could mix parameters and JSON request.
-                params: params
-            }
-
-            var endPoint = this.restBaseUrl + "select";
-            // track the post parameters.
-            // the Object assign will merge / copy source object to target
-            // object.
-            var trackPayload = Object.assign({"end_point" : endPoint},
-                                             postParams);
-            solr.track(trackPayload);
-            // the query url should be some thing like this: 
-            // - 'https://one.sites.leocorn.com/rest/searchApi/search',
-            // it is seems easier to use query parameters in a JSON request.
-            axios.post(endPoint, postParams)
-            .then(function(response) {
-
-                console.log(response.data);
-                self.totalHits = response.data.response.numFound;
-
-                self.results = response.data.response.docs;
-                console.log(self.results);
-
-                // check if we have facets in response.
-                // Object hasOwnProperty is like hasKey but more complex.
-                if(response.data.hasOwnProperty('facet_counts')) {
-                //self.facets = response.data.facets;
-                    self.facets = self.getReadyFacets(response.data.facet_counts.facet_fields);
-                }
-                //self.stats = self.facets[self.facets.length - 1].statistics;
-                //console.log("statistics: " + self.stats);
-                self.resultSummary = "Found " + self.totalHits + " docs in total!"
-                if(self.totalHits > 0) {
-                    console.log('total hits: ' + self.totalHits);
-                    //console.log(JSON.stringify(self.facets));
-                    //console.log(JSON.stringify(response.data.documents[0]));
-                    //console.log(response.data.documents[0].fields['title']);
-                }
-            })
-            .catch(function(error) {
-              self.resultSummary = "Query Error!";
-              console.log(error);
-            });
         },
 
         /**
@@ -441,13 +328,13 @@ export default {
      */
     created() {
 
-      this.eventQueues = this.$localSettings.solr.eventQueues;
-      this.restBaseUrl = this.eventQueues[0].url;
-      this.queueLabel = this.eventQueues[0].name;
+      this.collections = this.$localSettings.solr.eventQueues;
+      this.restBaseUrl = this.collections[0].url;
+      this.collectionLabel = this.collections[0].name;
       // set the tracking base url.
       solr.config.trackingBaseUrl = this.$localSettings.solrTrackingUrl;
 
-      this.loadEvents();
+      this.loadItems();
     }
 }
 </script>
