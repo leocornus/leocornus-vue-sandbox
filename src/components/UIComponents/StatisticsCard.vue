75f165a272abd4b87c01270b17c9756f8bf6d439
Sean Chen <sean.chen@leocorn.com>
2018-10-10 18:10:17 +0000

    get ready the statistics card component

---
+++ src/components/UIComponents/StatisticsCard.vue
@@ -0,0 +1,90 @@
+<template>
+<b-card no-body class="bg-primary mb-2" v-if="theStats">
+  <b-card-header class="bg-primary text-white">
+    <a v-b-toggle="statsCollapseID"
+       href="#" class="card-link text-white">
+      Statistics
+    </a>
+  </b-card-header>
+  <b-collapse :id="statsCollapseID">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center"
+        v-for="(item, index) in theStats" :key="index">
+      {{item.key}} : {{item.value}}
+    </li>
+  </ul>
+  </b-collapse>
+</b-card>
+</template>
+
+<script>
+// bootstrap-vue components.
+import bCard from 'bootstrap-vue/es/components/card/card'
+import bCardHeader from 'bootstrap-vue/es/components/card/card-header'
+import bCollapse from 'bootstrap-vue/es/components/collapse/collapse'
+import vBToggle from 'bootstrap-vue/es/directives/toggle/toggle'
+
+import * as d3 from 'd3'
+
+export default {
+
+    components: {
+      'b-card': bCard,
+      'b-card-header': bCardHeader,
+      'b-collapse': bCollapse
+    },
+
+    directives: {
+      // Vue will automatically prefix the directive name with 'v-'
+      // so we will use it like this: v-b-toggle.
+      'b-toggle': vBToggle
+    },
+
+    name: "stats-card",
+
+    props: ["stats"],
+
+    // using computed for dynamic data.
+    computed: {
+
+        /**
+         * compute the collapse id.
+         */
+        statsCollapseID() {
+            return "stats";
+        },
+
+        /**
+         * compute the statistics data.
+         * It will be an array of Objects with the follwing structure:
+         * { "key": "key-one",
+         *   "value": "value-one"}
+         */
+        theStats() {
+
+            var self = this;
+            if(self.stats) {
+                // using d3-format to format numbers.
+                var format = d3.format(",.2f");
+                var items = [];
+                Object.keys(self.stats).forEach(function(theKey) {
+                    var item = {
+                      "key": theKey,
+                      "value": format(self.stats[theKey])
+                    }
+                    items.push(item);
+                });
+
+                return items;
+            } else {
+                return [];
+            }
+        }
+    },
+
+    // methods.
+    methods: {
+
+    }
+}
+</script>
