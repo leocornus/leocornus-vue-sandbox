d908c36c55a160fc20c323ba98089fccbdff1dac
Sean Chen <sean.chen@leocorn.com>
2018-10-17 17:19:27 +0000

    create the Game Settings modal.

--- src/components/Dashboard/Views/GameBoard.vue
+++ src/components/Dashboard/Views/GameBoard.vue
@@ -2,6 +2,9 @@
 <div class="content container">
   <div id="game-app">
     <b-input-group class="mb-2">
+      <b-button variait="primary" v-b-modal.game-settings>Game Settings</b-button>
+    </b-input-group>
+    <b-input-group class="mb-2">
       <b-input-group-append>
         <span id="restBaseUrl-addon" class="input-group-text">Teams: </span>
       </b-input-group-append>
@@ -49,6 +52,16 @@
       <b-button variant="outline-primary">Submit</b-button>
     </b-input-group>
   </div>
+
+  <b-modal id="game-settings" title="Game Settings">
+    <p class="h4">Home Team</p>
+    <b-input-group prepend="Team Name:">
+      <b-form-input type="text" aria-label="Text input with radio button" />
+    </b-input-group>
+    <b-input-group prepend="Players: ">
+      <b-form-input type="text" aria-label="Text input with radio button" />
+    </b-input-group>
+  </b-modal>
 </div>
 </template>
 
@@ -66,9 +79,30 @@ export default {
             // TODO: we will collect teams's information from
             // modal.
             teams: [
-                {name: "home team"},
-                {name: "guest team"}
-            ],
+                {name: "home team",
+                 players: [
+                     {name: "name one", number: 7},
+                     {name: "name two", number: 12},
+                     {name: "name three", number: 23},
+                     {name: "name fore", number: 24},
+                     {name: "name five", number: 3},
+                     {name: "name six", number: 82},
+                     {name: "name seven", number: 15},
+                     {name: "name eight", number: 9}
+                 ]
+                },
+                {name: "guest team"},
+                 players: [
+                     {name: "name one", number: 7},
+                     {name: "name two", number: 12},
+                     {name: "name three", number: 23},
+                     {name: "name fore", number: 24},
+                     {name: "name five", number: 3},
+                     {name: "name six", number: 82},
+                     {name: "name seven", number: 15},
+                     {name: "name eight", number: 9}
+                 ]
+             ],
 
             // players.
             players: [
