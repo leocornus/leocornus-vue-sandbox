b21a9c18ec0bb13adb9468f2e23d48194c65eb00
Sean Chen <sean.chen@leocorn.com>
2018-10-29 16:43:55 +0000

    sorting by action name

--- src/components/Dashboard/Views/GameBoard.vue
+++ src/components/Dashboard/Views/GameBoard.vue
@@ -583,6 +583,21 @@ export default {
                     );
                 });
 
+                // sort by action name.
+                actions.sort(function(a, b) {
+                    var nameA = a.Action.toUpperCase(); // ignore upper and lowercase
+                    var nameB = b.Action.toUpperCase(); // ignore upper and lowercase
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+
+                    // names must be equal
+                    return 0;
+                });
+
                 vm.teamActions = actions;
             })
             .catch(function(error) {
