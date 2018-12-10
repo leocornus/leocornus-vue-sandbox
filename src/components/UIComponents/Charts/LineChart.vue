cdbfc4ab38673df26c3914b01617fcf3529a3b5a
Sean Chen <sean.chen@leocorn.com>
2018-12-10 17:18:52 +0000

    using the vue custom directive to binding axes it is a lot easier

--- src/components/UIComponents/Charts/LineChart.vue
+++ src/components/UIComponents/Charts/LineChart.vue
@@ -1,56 +1,62 @@
 <template>
-  <svg width="500" height="270" :id="svgId">
-    <g style="transform: translate(0, 10)">
+  <svg width="500" height="270">
+    <g transform="translate(0, 10)">
       <path :d="line" />
     </g>
+    <g v-axis:x="scale" transform="translate(0, 25)"></g>
   </svg>
 </template>
 
 <script>
 import * as d3 from 'd3';
 
+// define he properties for this component here.
+
 export default {
+
   name: 'line-chart',
+
   data() {
     return {
-      svgId: 'abc',
-      data: [99, 71, 78, 25, 36, 92],
-      line: '',
+      data: [99, 71, 78, 25, 36, 92]
     };
   },
-  mounted() {
-    this.calculatePath();
-  },
-  methods: {
 
-    getScales() {
+  // check the vue.js doc about custom directives:
+  // - https://vuejs.org/v2/guide/custom-directive.html
+  directives: {
+
+      axis(el, binding) {
+
+          const axis = binding.arg;
+          const axisMethod = { x: "axisBottom", y: "axisLeft" }[axis];
+          const methodArg = binding.value[axis];
+
+          d3.select(el).call(d3[axisMethod](methodArg));
+      }
+  },
+
+  computed: {
+
+    scale() {
       const x = d3.scaleTime().range([0, 430]);
       const y = d3.scaleLinear().range([210, 0]);
 
-      d3.axisLeft().scale(x);
-      d3.axisBottom().scale(y);
-
       x.domain(d3.extent(this.data, (d, i) => i));
       y.domain([0, d3.max(this.data, d => d)]);
+
       return { x, y };
     },
 
-    calculatePath() {
-      const scale = this.getScales();
-      const path = d3.line()
-        .x((d, i) => scale.x(i))
-        .y(d => scale.y(d));
-      this.line = path(this.data);
+    line() {
 
-      d3.select('#' + this.svgId)
-          .append("g")
-          // the powerful transform, details from this page:
-          // - https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform
-          .attr("transform", "translate(0, 250)")
-          // call function will work with a D3 selection
-          .call(d3.axisBottom(scale.x));
-    },
-  },
+      const path = d3.line()
+        .x((d, i) => this.scale.x(i))
+        .y(d => this.scale.y(d));
+
+      return path(this.data);
+    }
+  }
 };
 </script>
 
