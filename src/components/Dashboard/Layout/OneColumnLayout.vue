235edb2eb59d53cf6c4329aedbdc40b6db754eb4
Sean Chen <sean.chen@leocorn.com>
2019-01-14 14:24:36 +0000

    add the one column layout

---
+++ src/components/Dashboard/Layout/OneColumnLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click="toggleSidebar">
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+  </div>
+</template>
+<style lang="scss">
+
+</style>
+<script>
+  import TopNavbar from './TopNavbar.vue'
+  import ContentFooter from './ContentFooter.vue'
+  import DashboardContent from './Content.vue'
+  export default {
+    computed: {
+    },
+    components: {
+      TopNavbar,
+      ContentFooter,
+      DashboardContent,
+    },
+    methods: {
+      toggleSidebar () {
+        if (this.$sidebar.showSidebar) {
+          this.$sidebar.displaySidebar(false)
+        }
+      }
+    }
+  }
+
+</script>
