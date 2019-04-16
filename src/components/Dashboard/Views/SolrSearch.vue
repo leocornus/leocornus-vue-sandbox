d8bb4e7000f11ffaf52494181792db85479329a7
Sean Chen <sean.chen@leocorn.com>
2019-04-16 13:13:25 +0000

    switch back to table view

--- src/components/Dashboard/Views/SolrSearch.vue
+++ src/components/Dashboard/Views/SolrSearch.vue
@@ -82,15 +82,15 @@
           </b-row>
           <!-- results-list :docs="results" v-if="results">
           </results-list -->
-          <product-preview v-for="(doc, index) in results"
+          <!-- product-preview v-for="(doc, index) in results"
               :doc="doc" :key="doc.id"
               :index="index" :idFieldName="idField" :thePage="page"
               v-on:show-details="showItemDetails">
-          </product-preview>
-          <!-- listing-details-table :docs="results" v-if="results" :fieldList="fieldList"
+          </product-preview -->
+          <listing-details-table :docs="results" v-if="results" :fieldList="fieldList"
                                  :idFieldName="idField" :thePage="page"
                                  v-on:show-details="showItemDetails">
-          </listing-details-table -->
+          </listing-details-table>
 
           <b-pagination :total-rows="totalHits" :per-page="perPage" v-if="results"
                         v-model="currentPage" align="right"></b-pagination>
@@ -215,16 +215,16 @@ import bInputGroupAppend from 'bootstrap-vue/es/components/input-group/input-gro
 import bFormInput from 'bootstrap-vue/es/components/form-input/form-input'
 
 import axios from 'axios'
-//import ListingDetailsTable from '@/components/UIComponents/ListingDetailsTable.vue'
-import ProductPreview from '@/components/UIComponents/ProductPreview.vue'
+import ListingDetailsTable from '@/components/UIComponents/ListingDetailsTable.vue'
+//import ProductPreview from '@/components/UIComponents/ProductPreview.vue'
 import FacetBuckets from '@/components/UIComponents/FacetBuckets.vue'
 
 import solr from '@/libs/solr'
 
 export default {
     components: {
-      //'listing-details-table': ListingDetailsTable,
-      'product-preview': ProductPreview,
+      'listing-details-table': ListingDetailsTable,
+      //'product-preview': ProductPreview,
       'facet-buckets': FacetBuckets,
       'b-input-group': bInputGroup,
       'b-input-group-append': bInputGroupAppend,
