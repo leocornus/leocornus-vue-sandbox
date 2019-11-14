14a9f5e43d72e860ffbfc2f9d3b5fa85532f742b
Sean Chen <sean.chen@leocorn.com>
2019-11-14 16:15:46 +0000

    add comments.

--- src/components/UIComponents/FacetBuckets.vue
+++ src/components/UIComponents/FacetBuckets.vue
@@ -64,7 +64,7 @@ export default {
 
         // ids for collapse.
         facetID() {
-            // replace the white space with -.
+            // TODO: replace the white space with -.
             return this.facet.name;
         },
 
