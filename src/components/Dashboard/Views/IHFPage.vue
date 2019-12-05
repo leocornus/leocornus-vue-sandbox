55899eaf06fb6f9c9977d17bd564174a42428ad3
Sean Chen <sean.chen@leocorn.com>
2019-12-05 15:46:13 +0000

    Get started the IHF page.

---
+++ src/components/Dashboard/Views/IHFPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <solr-page :pageName="name">
+  </solr-page>
+</template>
+
+<script>
+import SolrPage from '@/components/UIComponents/SolrPage.vue'
+
+export default {
+
+    components: {
+      'solr-page': SolrPage
+    },
+
+    data() {
+      return {
+        name: "logging"
+      }
+    }
+}
+</script>
